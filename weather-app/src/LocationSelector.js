1a0ba653cc44f0b1d6304f73deada95adf572053
jessica73 <jessica_h_73@outlook.com>
2018-04-01 22:54:19 -0700

    remove refresh
    remove refresh button. doesn't make sense if dropdown option changed but don't click refresh, then forecast is misleading - either display location or remove refresh option

--- weather-app/src/LocationSelector.js
+++ weather-app/src/LocationSelector.js
@@ -9,20 +9,15 @@ class LocationSelector extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
         this.setState({
             location: e.target.value
+        }, () => {
+            // pass selected location back to main component
+            this.props.onSelectLocation(this.state.location);
         });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        
-        // pass selected location back to main component
-        this.props.onSelectLocation(this.state.location);
     }
 
     /*
@@ -30,24 +25,20 @@ class LocationSelector extends Component {
      */
     render() {
         return (
-            <div style={{ paddingTop: '10px' }}>
-                <form onSubmit={this.handleSubmit}>
-                    <select value={this.state.location} onChange={this.handleChange}>
-                        <option value="">-- Select a City --</option>
-                        <option value="melbourne, au">1. Melboure, Australia</option>
-                        <option value="vienna, at">2. Vienna, Austria</option>
-                        <option value="vancouver, bc">3. Vancouver, Canada</option>
-                        <option value="toronto, on">4. Toronto, Canada</option>
-                        <option value="calgary, ab">5. Calgary, Canada (tie)</option>
-                        <option value="adelaide, au">5. Adelaide, Australia (tie)</option>
-                        <option value="perth, au">7. Perth, Australia</option>
-                        <option value="auckland, nz">8. Auckland, New Zealand</option>
-                        <option value="helsinki, fi">9. Helsinki, Finland</option>
-                        <option value="hamburg, de">10. Hamburg, Germany</option>
-                    </select>
-                    &nbsp;
-                    <input type="submit" value="Refresh" />
-                </form>
+            <div style={{ paddingTop: '10px' }}>                
+                <select value={this.state.location} onChange={this.handleChange}>
+                    <option value="">-- Select a City --</option>
+                    <option value="melbourne, au">1. Melboure, Australia</option>
+                    <option value="vienna, at">2. Vienna, Austria</option>
+                    <option value="vancouver, bc">3. Vancouver, Canada</option>
+                    <option value="toronto, on">4. Toronto, Canada</option>
+                    <option value="calgary, ab">5. Calgary, Canada (tie)</option>
+                    <option value="adelaide, au">5. Adelaide, Australia (tie)</option>
+                    <option value="perth, au">7. Perth, Australia</option>
+                    <option value="auckland, nz">8. Auckland, New Zealand</option>
+                    <option value="helsinki, fi">9. Helsinki, Finland</option>
+                    <option value="hamburg, de">10. Hamburg, Germany</option>
+                </select>                
             </div>
         );
     }
